02de10aa5bf5fd60ada5c8789f7d9c248427dfec
Endzelisp <endzelisp@gmail.com>
2022-11-12 19:44:00 -0400

    Improve book-cards and new book form design

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const newBookBtn = document.querySelector('div.wrapper > header > button');
 const modalForm = document.querySelector('div.form-modal');
 const closeModal = document.querySelector('div.form-modal > form > span');
 
+const formFieldset = document.querySelector('div.form-modal > form > fieldset');
+const addBookBtn = document.querySelector('div.form-modal > form > button');
+const titleInput = formFieldset.querySelector('input[id="book-title"]');
+const authorInput = formFieldset.querySelector('input[id="book-author"]');
+const pagesInput = formFieldset.querySelector('input[id="book-pages"]');
+
 function toggleModal () {
   modalForm.classList.toggle('visible');
 }
